a6ef9c80277ad50ae3207b4f44d1b112e07f9e08
Abhinav Agrawal <abhinavagrawal1995@gmail.com>
2018-11-23 18:06:31 -0500

    adding CI script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movie-booking'`, () => {
+  it(`should have as title 'bookit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('movie-booking');
+    expect(app.title).toEqual('bookit');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to movie-booking!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bookit!');
   });
 });
